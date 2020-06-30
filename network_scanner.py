495cce4acf3ac229f0969dc2c1ec3e6af3dbb166
dashzou <fxfddfno@gmail.com>
2020-06-30 10:55:32 -0400

    semd arp requst and create broadcast

--- network_scanner.py
+++ network_scanner.py
@@ -4,7 +4,20 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_boradcast = broadcast/arp_request
+
+    print(arp_request.summary())
+    print(broadcast.summary())
+    print(arp_request_boradcast.summary())
+
+    arp_request.show()
+    broadcast.show()
+    arp_request_boradcast.show()
+
+    # show all feilds & informations
+    scapy.ls(scapy.Ether())
 
 
-scan("192.168.57.2/24")
+scan(("192.168.57.1"))
